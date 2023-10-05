7162ac7afe7869becaeea329f7870bae15eed53c
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2023-10-05 17:11:48 +0200

    Update: Remove USART/Serial activity from sleep example

--- examples/6. Others/STM32WL-Lowpower/STM32WL-Lowpower.ino
+++ examples/6. Others/STM32WL-Lowpower/STM32WL-Lowpower.ino
@@ -10,39 +10,36 @@
  *
  * This example demonstrates how to set the MCU/Board in Sleep mode and waking up with RTC alarm.
  * Please install these library to proceed.
- * 
+ *
  * STM32LowPower: https://github.com/stm32duino/STM32LowPower
  * STM32RTC: https://github.com/stm32duino/STM32RTC
- * 
+ *
  */
 
 #include <STM32LowPower.h> // https://github.com/stm32duino/STM32LowPower
 #include <STM32RTC.h>      // https://github.com/stm32duino/STM32RTC
 
 STM32RTC &rtc = STM32RTC::getInstance();
+rfthings_sx126x sx126x;
+rft_status_t status;
 
 void setup(void)
 {
   rtc.begin();
 
-  Serial.begin(115200);
-  while (!Serial)
-  {
-  }
-  Serial.print("STM32WL Low Power + RTC Example");
-
   LowPower.begin();
   LowPower.enableWakeupFrom(&rtc, alarmMatch);
+
+  // Init SX126x
+  status = sx126x.init(RFT_REGION_EU863_870);
 }
 
 void loop(void)
 {
-  Serial.println("Loop");
-  Serial.flush();
-
   delay(2000); // Stay awake for 2 seconds
 
   rtc.setAlarmEpoch(rtc.getEpoch() + 5); // Set an alarm to wake up after 5 seconds
+  sx126x.sleep();
   LowPower.deepSleep();
 }
 
